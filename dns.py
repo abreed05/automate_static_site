4644071ff833a3bfd866dca55891c7139ff95694
Aaron Breeden <39411883+abreed05@users.noreply.github.com>
2021-01-19 15:54:53 -0500

    Update dns.py
    Modified script to clean up the text received from AWS, thus eliminating the need for a secondary bash script.

--- dns.py
+++ dns.py
@@ -1,55 +1,57 @@
-#!/usr/bin/python3
-
-import re
+import json
 import os
-import subprocess
-import time
+import re
+import requests
 # imports config.py to use the API key for Godaddy
 import config
 
-# A function to call the cleanup.sh script
-def call_bash_script():
-    subprocess.call("./cleanup.sh", shell=True)
-    time.sleep(10)
-
-# Function to actually update the DNS servers at godaddy
+# Retireve DNS records, clean up the text and push dns records update to GoDaddy
 def update_dns_fn():
 
-    # gdcreds creates the API string from config.py that the curl command below will need to interact with the godaddyapi
-    gdcreds = config.api_key + ":" + config.api_secret
+    # Pull Records from Hosted Zone
+    hz_id = "aws route53 list-hosted-zones-by-name > hzid.json"
+    os.system(hz_id)
+    with open('hzid.json') as json_file:
+        data = json.load(json_file)
+        for p in data['HostedZones']:
+            JID = re.split(r'/hostedzone/', p['Id'])
 
-    # gd_domain is the domain you want to modify. 
-    gd_domain = yourdomain.com
+    # Pull NS records from Hosted Zone
+    ns_records = "aws route53 list-resource-record-sets --hosted-zone-id " + JID[1] + " --output json > dns_records.txt"
+    os.system(ns_records)
+    with open('dns_records.txt') as json2:
+        data2 = json.load(json2)
+        p = data2['ResourceRecordSets']
+        p = p[0]
+        p = p['ResourceRecords']
+        with open('text', 'w') as filehandle:
+            filehandle.writelines("%s\n" % line for line in p)   
 
-    # read the dns_records.txt file created by cleanup.sh 
-    f = open('dns_records.txt', 'r')
-    lines = f.readlines()[-4:]
-    f.close()
+     # Clean up the NS records text file
+    filename = 'text'
+    file = open(filename, 'rt')
+    text = file.read()
+    file.close()
+    words = re.split(r'\s+', text)
+    split1 = words[1].strip('\'\}\.')
+    split2 = words[3].strip('\'\}\.')
+    split3 = words[5].strip('\'\}\.')
+    split4 = words[7].strip('\'\}\.')
 
-    # char_list strips bad characters in the dns_records.txt file. Basically more cleanup of the text
-    char_list = ["'", "\[", "\]"]
+  # gdcreds creates the API string from config.py to pass into the requests module.
+    gdcreds = 'sso-key '+ config.api_key + ":" + config.api_secret
 
-    # split1 - 4 assigns a line from the index of lines and strips away all characters that are not in the ns format
-    split1 = lines[0].strip().split()
-    split1 = re.sub("|".join(char_list), "", str(split1))
-    split2 = lines[1].strip().split()
-    split2 = re.sub("|".join(char_list), "", str(split2))
-    split3 = lines[2].strip().split()
-    split3 = re.sub("|".join(char_list), "", str(split3))
-    split4 = lines[3].strip().split()
-    split4 = re.sub("|".join(char_list), "", str(split4))
+ # create the API HTTP PUT request
+    godaddy_url = "https://api.godaddy.com/v1/domains/" + config.domainname + "/records/NS/"
+    gd_headers = {'Content-Type': 'application/json', 'accept': 'application/json', 'Authorization': gdcreds}
+    payload = [{'data': str(split1), 'name': config.domainname, 'ttl': 3600}, {'data': str(split2), 'name': config.domainname', 'ttl': 3600}, {'data': str(split3), 'name': config.domainname', 'ttl': 3600}, {'data': str(split4), 'name': config.domainname, 'ttl': 3600}]
+    # Make the request
+    requests.put(godaddy_url, headers=gd_headers, json=payload)
 
-    # update_dns prepare the command to be called. This is just a lot of strings added together to make one complete curl command. 
-    update_dns = 'curl -X PUT \"https://api.godaddy.com/v1/domains/' + gd_domain + '/records/NS/%40\" -H  \"accept: application/json\" -H  \"Content-Type: application/json\" -H  \"Authorization: sso-key ' + gdcreds + '\" -d \"[  {\\"data\\": \\"' + str(split1) + '\\",' + ' \\"ttl\\": 3600},  {\\"data\\": \\"' + str(split2) + '\\", \\"ttl\\": 3600}, {\\"data\\": \\"' + str(split3) + '\\", \\"ttl\\": 3600}, {\\"data\\": \\"' + str(split4) + '\\", \\"ttl\\": 3600}]\"'
-    
-    # Actually calls the curl command through the OS
-    os.system(update_dns)
-
-# A cleanup function to remove the tmp files created by cleanup.sh
+# Cleanup function to remove the tmp files created by cleanup.sh
 def cleanup_fn():
-    cleanup = "rm dns_records.txt hzid.txt"
+    cleanup = "rm dns_records.txt hzid.json text"
     os.system(cleanup)
 
-call_bash_script()
 update_dns_fn()
 cleanup_fn()
